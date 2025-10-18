3ebc355cf2c4f53f62c45846b4e5854b0f84ac1b
Wes Higbee <wes.mcclure@gmail.com>
2025-10-18 17:11:33 -0500

    use explicit quantization_config

--- experiments/1-qwen25coder/test.py
+++ experiments/1-qwen25coder/test.py
@@ -1,10 +1,17 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+
+bnb_config = BitsAndBytesConfig(load_in_8bit=True)
 
 model_name = "Qwen/Qwen2.5-Coder-0.5B"
 tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
-    load_in_8bit=True,
+    quantization_config=bnb_config,
     device_map="auto",
     trust_remote_code=True
 )
+
+# %%
+
+
+
