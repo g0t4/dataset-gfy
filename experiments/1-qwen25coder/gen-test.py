78b85561345ec92117e3facc68cea6611862ed77
Wes Higbee <wes.mcclure@gmail.com>
2025-10-22 14:47:49 -0500

    rename so I can split out a separate variable for the fine tuned model... and not use 'model' anymore to avoid confusion

--- experiments/1-qwen25coder/gen-test.py
+++ experiments/1-qwen25coder/gen-test.py
@@ -15,14 +15,14 @@ model_name = "Qwen/Qwen2.5-Coder-0.5B"
 tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 assert tokenizer.pad_token is not None
 
-model = AutoModelForCausalLM.from_pretrained(
+original_model = AutoModelForCausalLM.from_pretrained(
     model_name,
     # torch_dtype="auto",  # or torch.bfloat16
     torch_dtype=torch.bfloat16,
     trust_remote_code=True,
     # attn_implementation="flash_attention_2" ... LATER
 )
-model.to(use_device)
+original_model.to(use_device)
 
 # %%
 
@@ -30,9 +30,9 @@ test = "roses are red, violets"
 # TODO after the fine tune, see how it answers this same prompt... if it says smth unexpected (esp. that rhymes) then I know the fine tune is working!
 
 for i in range(1, 10):
-    inputs = tokenizer(test, return_tensors="pt").to(model.device)
+    inputs = tokenizer(test, return_tensors="pt").to(original_model.device)
     inputs.input_ids
-    response = model(**inputs)
+    response = original_model(**inputs)
     logits = response.logits
     logits.shape
     last = logits[0,-1:][0]
@@ -51,8 +51,8 @@ prompt_recursion = "Explain recursion simply:"
 
 # %%
 
-model.config.use_cache = False  # must be False for gradient checkpointing
-model.gradient_checkpointing_enable()
+original_model.config.use_cache = False  # must be False for gradient checkpointing
+original_model.gradient_checkpointing_enable()
 
 # FYI for now, use all examples to verify fine tune is operational... then later split out test/train and reset adapter/model
 train_ds = load_dataset("json", data_files="../../out/gfy.nocomments.jsonl")["train"]
@@ -70,7 +70,7 @@ tokenized = train_ds.map(format)
 def dump_model_info(when, model):
     for name, param in list(model.named_parameters())[:10]:
         rich.print(f"{when} {name:40} {param.device} {param.dtype}")
-dump_model_info("before train", model) # shows bfloat16 on my nvidia GPU
+dump_model_info("before train", original_model) # shows bfloat16 on my nvidia GPU
 
 
 # %%
@@ -86,10 +86,10 @@ config = LoraConfig(
 
 # %%
 
-model = get_peft_model(model, config)
+original_model = get_peft_model(original_model, config)
 # dump_model_info("lora before bf16 to", model) # shows float32! for lora layers
-model.to(torch.bfloat16)
-dump_model_info("lora after bf16 to", model) # shows bfloat16 now
+original_model.to(torch.bfloat16)
+dump_model_info("lora after bf16 to", original_model) # shows bfloat16 now
 
 from transformers import DataCollatorForLanguageModeling
 
@@ -113,7 +113,7 @@ args = TrainingArguments(
 )
 
 trainer = Trainer(
-    model=model,
+    model=original_model,
     args=args,
     train_dataset=tokenized,
     # data_collator=collator,
@@ -125,9 +125,9 @@ trainer.train()
 
 # use pipeline to infer
 def compare(prompt):
-    model.eval()
-    untrained = pipeline("text-generation", model=model, tokenizer=tokenizer, device=0, torch_dtype=torch.bfloat16)
-    finetuned = pipeline("text-generation", model=model, tokenizer=tokenizer, device=0, torch_dtype=torch.bfloat16)
+    original_model.eval()
+    untrained = pipeline("text-generation", model=original_model, tokenizer=tokenizer, device=0, torch_dtype=torch.bfloat16)
+    finetuned = pipeline("text-generation", model=original_model, tokenizer=tokenizer, device=0, torch_dtype=torch.bfloat16)
     # warm up the pipeline
     rich.print("\n[bold yellow]warmup untrained[/]")
     rich.print(untrained("warmup")[0]["generated_text"])
@@ -147,13 +147,13 @@ compare("Explain why humans have a sense of self:")
 
 # manual inference (like mine above)
 
-model.eval()
+original_model.eval()
 prompt = "roses are red, violets"
 inputs = tokenizer(prompt, return_tensors="pt").to("cuda:0")
 
 with torch.no_grad():
     for i in range(10):
-        outputs = model(**inputs)
+        outputs = original_model(**inputs)
         next_id = outputs.logits[0, -1].argmax().unsqueeze(0)
         inputs = {
             "input_ids": torch.cat([inputs["input_ids"], next_id.unsqueeze(0)], dim=1),
