94a6d1e398eb2df8ab5cab5a065f896a358ef1b2
Wes Higbee <wes.mcclure@gmail.com>
2025-10-24 11:28:40 -0500

    kata - practice manual chat prompt formatting using template

--- experiments/uncensor/vanilla.py
+++ experiments/uncensor/vanilla.py
@@ -13,18 +13,18 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH_CHAT, trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained(MODEL_PATH_CHAT, trust_remote_code=True,
+chat_model = AutoModelForCausalLM.from_pretrained(MODEL_PATH_CHAT, trust_remote_code=True,
     # fp16=True, # TODO! figure out what is native for this model (IIUC its bf16)
 )
-model.to(DEVICE)
-model.eval()
+chat_model.to(DEVICE)
+chat_model.eval()
 # ImportError: cannot import name 'BeamSearchScorer' from 'transformers' (.venv/lib/python3.12/site-packages/transformers/__init__.py)
 #  apparently this was removed in transformers... so I need to use a different
 
 # %%
 
-def manual_inference(model, text):
-    for i in range(1, 10):
+def manual_inference(model, text, max_tokens=10):
+    for _ in range(max_tokens):
         inputs = tokenizer(text, return_tensors="pt").to(model.device)
         inputs.input_ids
         response = model(**inputs)
@@ -36,7 +36,10 @@ def manual_inference(model, text):
         #             TODO switch to not re-encode all tokens on every iteration
         next = tokenizer.decode(max_token_id_next, skip_special_tokens=True)
         text = text + next
-        print(text)
+        # check EOS
+    print(text)
+
+# %%
 
 def test_base():
     base_tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH_BASE, trust_remote_code=True)
@@ -49,12 +52,34 @@ test_base()
 
 # %%
 
-model.chat(query="what is a cuck?", history=[], tokenizer=tokenizer)
-
+chat_model.chat(query="what is a cuck?", history=[], tokenizer=tokenizer)
 
 # %%
 
-pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, device=DEVICE)
+# test manual chat prompt
+
+# TEMPLATE from qwen repo
+#
+# {% for message in messages %}
+# {% if loop.first and message['role'] != 'system' %}{{ '<|im_start|>system\nYou are a helpful assistant.<|im_end|>\n' }}{% endif %}
+# {{ '<|im_start|>' + message['role'] + '\n' + message['content'] + '<|im_end|>\n' }}
+# {% if loop.last and add_generation_prompt %}{{ '<|im_start|>assistant\n' }}{% endif %}
+# {% endfor %}
+
+def manual_chat_inference(model, query):
+    prompt = f"""<|im_start|>system\nYou are a helpful assistant.<|im_end|>
+<|im_start|>user
+{query}<|im_end|>
+<|im_start|>assistant
+"""
+    manual_inference(model, prompt, max_tokens=100)
+
+
+manual_chat_inference(chat_model, "what is the most popular programming language?")
+
+# %%
+
+pipe = pipeline("text-generation", model=chat_model, tokenizer=tokenizer, device=DEVICE)
 response = pipe("test")
 
 print(response)
