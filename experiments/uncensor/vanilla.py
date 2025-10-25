f1a9ccb46545c44d0a8480a27d24dca2a73e5e5c
Wes Higbee <wes.mcclure@gmail.com>
2025-10-25 02:41:24 -0500

    recompute next token ID off of final hidden state, works!

--- experiments/uncensor/vanilla.py
+++ experiments/uncensor/vanilla.py
@@ -37,36 +37,63 @@ def summarize_layer(name, param):
     print(f"{name} shape={tuple(param.shape)} {param.dtype}")
 
 for name, param in chat_model.named_parameters():
-    if "wte" in name:
-        summarize_layer(name, param)
+    # if "wte" in name:
+    summarize_layer(name, param)
 
-for m in chat_model.modules():
-    print("---")
-    print(m)
+# for m in chat_model.modules():
+#     print("---")
+#     print(m)
 
 # %% * inspect hiddens w/o transformer_lens HookedTransformer
 
 def inspect_hiddens_in_forward_pass(tokenizer, model, text, max_tokens=1):
     for _ in range(max_tokens):
         inputs = tokenizer(text, return_tensors="pt").to(model.device)
-        inputs.input_ids
+        print("inputs", inputs)
+        summarize_layer("inputs.input_ids", inputs.input_ids)
         response = model(**inputs, output_hidden_states=True)
         # TODO look into what I can get at with just output_hidden_states and not Transformer Lens
         #   IIUC won't get names here
         #   also obviously can't modify, only inspect
         logits = response.logits
+        summarize_layer("logits", logits)
         logits.shape
         last = logits[0, -1:][0]
         max_token_id_next = last.argmax()
+        print("max_token_id_next", max_token_id_next)
         if max_token_id_next.item() == tokenizer.eos_token_id:
             break
         #             "input_ids": torch.cat([inputs["input_ids"], next_id.unsqueeze(0)], dim=1),
         #             TODO switch to not re-encode all tokens on every iteration
         next = tokenizer.decode(max_token_id_next, skip_special_tokens=True)
         text = text + next
+
+
+        # ** recompute next token off final hidden state + weights to map hidden space => token space
         # print("hidden states:", response.hidden_states)
         for number, hidden in enumerate(response.hidden_states):
             summarize_layer(f"hidden: layer {number}", hidden)
+        # TODO take final hidden layer and matmul wte.T to re-create the logits and see if they line up
+        print("REDO LOGITS off last hidden:")
+        last_hidden = response.hidden_states[-1]
+        # last_hidden = last_hidden.squeeze()[-1]
+        # wte = model.transformer.wte.weight # this must just be for input side (does not match)
+        # summarize_layer("  wte", wte)
+        weights_lm_head = model.lm_head.weight  # this results in a match!
+        summarize_layer("  weights_lm_head", weights_lm_head)
+        #    FYI dump named_parameters (layers) and that's how I found lm_head at end with the right dimension
+        last_tok_last_hidden = last_hidden[:, -1:, :]
+        summarize_layer("  last_tok_last_hidden", last_tok_last_hidden)
+        hidden_vector_last_token = last_tok_last_hidden.squeeze()
+        summarize_layer("  hidden_vector_last_token", hidden_vector_last_token)  # remove both 1-D dimensions
+        redo_logits = hidden_vector_last_token.matmul(weights_lm_head.T)
+        # redo_logits = weights_lm_head.matmul(hidden_vector_last_token) # s/b same result
+        summarize_layer("  redo_logits", redo_logits)
+        redo_max_token_id_next = redo_logits.argmax()
+        print("  redo max_token_id_next:", redo_max_token_id_next)
+        redo_decoded = tokenizer.decode(redo_max_token_id_next, skip_special_tokens=True)
+        print("  redo decoded: '" + redo_decoded + "'")
+
     print(text)
     return text
 
@@ -155,7 +182,8 @@ for n in base_model.parameters():
 
 # %%
 
-# * model visualizer! using these layers
-# compare named_parameters to transformer_lens's hooks
-for name, param in base_model.named_parameters():
-    summarize_layer(name, param)
+def summarize_named_params(model):
+    for name, param in model.named_parameters():
+        summarize_layer(name, param)
+
+summarize_named_params(base_model)
