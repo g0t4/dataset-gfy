974e9aade0e10a808546110b58ff02b01fd71d78
Wes Higbee <wes.mcclure@gmail.com>
2025-10-26 00:55:31 -0500

    pi

---
+++ experiments/uncensor/kata/pi.py
@@ -0,0 +1,8 @@
+import math
+
+math.pi
+
+math.cos(0)  # i.e. parallel vectors, pointing in same direction have (0 degrees)
+math.cos(math.pi)  # parallel vectors, pointed in opposite directions (180 degrees)
+
+# TODO later when I get to scaling context beyond training size using frequency shift
